34f3d6d595bc55dd45f6edc152c4b403dccdcaaa
takahiro.tominaga <tt.300607@gmail.com>
2022-01-20 08:41:23 +0900

    face project

---
+++ FaceAuthenticator/FaceAuthenticator/FaceAuthenticatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceAuthenticatorApp.swift
+//  FaceAuthenticator
+//
+//  Created by Takahiro Tominaga on 2022/01/20.
+//
+
+import SwiftUI
+
+@main
+struct FaceAuthenticatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ios-app
+
+[calculator](https://www.youtube.com/watch?v=cMde7jhQlZI&ab_channel=iOSAcademy)
+[face login](https://www.youtube.com/watch?v=qW1wQwHmoTI&ab_channel=DesignCode)

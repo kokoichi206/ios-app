f88cd086cfa38748d24e67790e05b8a91f40ed6b
takahiro.tominaga <tt.300607@gmail.com>
2022-01-19 23:45:29 +0900

    create calc project

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Takahiro Tominaga on 2022/01/19.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculator/Calculator/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Takahiro Tominaga on 2022/01/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            // Vertical Stack
+            VStack {
+                // Text display
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

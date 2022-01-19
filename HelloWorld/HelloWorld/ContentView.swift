e4ad5b4f35c4b53bbd2a8cbcd5bffebfe198df88
takahiro.tominaga <tt.300607@gmail.com>
2022-01-19 23:30:57 +0900

    my first iOS app

---
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by Takahiro Tominaga on 2022/01/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Takahiro Tominaga on 2022/01/19.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

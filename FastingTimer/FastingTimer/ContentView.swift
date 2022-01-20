29234ba0abcb0154a8142990ee2ce503cba60369
takahiro.tominaga <tt.300607@gmail.com>
2022-01-21 01:02:06 +0900

    implemented ui

--- FastingTimer/FastingTimer/ContentView.swift
+++ FastingTimer/FastingTimer/ContentView.swift
@@ -20,11 +20,70 @@ struct ContentView: View {
     }
     
     var content: some View {
-        VStack {
-            // MARK: Progress Ring
-            
-            ProgressRing()
+        ZStack {
+            VStack(spacing: 40) {
+                // MARK: Title
+                
+                Text("Let's get started!")
+                    .font(.headline)
+                    .foregroundColor(Color(#colorLiteral(red: 0, green: 0.3689519167, blue: 0.4431474507, alpha: 1)))
+                
+                // MARK: Fasting Plan
+                
+                Text("16:8")
+                    .fontWeight(.semibold)
+                    .padding(.horizontal, 24)
+                    .padding(.vertical, 8)
+                    .background(.thinMaterial)
+                    .cornerRadius(20)
+             
+                Spacer()
+            }
+            .padding()
+                
+            VStack(spacing: 40) {
+                // MARK: Progress Ring
+                
+                ProgressRing()
+                
+                HStack(spacing: 60) {
+                    // MARK: Start Time
+                    
+                    VStack(spacing: 5) {
+                        Text("Start")
+                            .opacity(0.7)
+                        
+                        Text(Date(), format: .dateTime.weekday().hour().minute().second())
+                            .fontWeight(.bold)
+                    }
+                    
+                    // MARK: End Time
+                    
+                    VStack(spacing: 5) {
+                        Text("End")
+                            .opacity(0.7)
+                        
+                        Text(Date().addingTimeInterval(16), format: .dateTime.weekday().hour().minute().second())
+                            .fontWeight(.bold)
+                    }
+                }
+                
+                // MARK: Button
+                
+                Button {
+                    
+                } label: {
+                    Text("Start fasting")
+                        .font(.title3)
+                        .padding(.horizontal, 24)
+                        .padding(.vertical, 8)
+                        .background(.thinMaterial)
+                        .cornerRadius(20)
+                }
+            }
+            .padding()
         }
+        .foregroundColor(.white)
     }
 }
 

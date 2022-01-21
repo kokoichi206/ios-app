b5a96928d6793843f345cb0c48b2e435667f95e6
takahiro.tominaga <tt.300607@gmail.com>
2022-01-21 21:19:50 +0900

    Create new project

---
+++ TaskManagement/TaskManagement/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TaskManagement
+//
+//  Created by Takahiro Tominaga on 2022/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TaskManagement/TaskManagement/TaskManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementApp.swift
+//  TaskManagement
+//
+//  Created by Takahiro Tominaga on 2022/01/21.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

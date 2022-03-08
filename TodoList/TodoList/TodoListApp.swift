1333543871b93200cd1fbd92190240c07a06d0a2
takahiro.tominaga <tt.300607@gmail.com>
2022-03-08 19:40:51 +0900

    first commit

---
+++ TodoList/TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Takahiro Tominaga on 2022/03/08.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

18ff4ce4abd8d59b37452a76c7d6dc93ad12a8f5
takahiro.tominaga <tt.300607@gmail.com>
2022-03-10 21:36:11 +0900

    first commit

---
+++ movie/movie/movieApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieApp.swift
+//  movie
+//
+//  Created by Takahiro Tominaga on 2022/03/10.
+//
+
+import SwiftUI
+
+@main
+struct movieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

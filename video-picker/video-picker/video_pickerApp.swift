1c33d1394e048f5831df820d3b6d2ecb3b3e1354
takahiro.tominaga <tt.300607@gmail.com>
2022-03-18 21:27:31 +0900

    first commit

---
+++ video-picker/video-picker/video_pickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  video_pickerApp.swift
+//  video-picker
+//
+//  Created by Takahiro Tominaga on 2022/03/18.
+//
+
+import SwiftUI
+
+@main
+struct video_pickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

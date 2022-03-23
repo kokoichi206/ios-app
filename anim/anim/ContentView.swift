1236af5c240a2092e3c17202c1b86854f28bb63f
takahiro.tominaga <tt.300607@gmail.com>
2022-03-23 20:41:20 +0900

    initial commit

---
+++ anim/anim/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  anim
+//
+//  Created by Takahiro Tominaga on 2022/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

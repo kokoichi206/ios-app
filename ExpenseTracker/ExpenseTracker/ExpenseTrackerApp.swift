f42633810abaa99b554080e5ee24de847dc191d4
takahiro.tominaga <tt.300607@gmail.com>
2022-03-29 00:31:28 +0900

    fetch data

--- ExpenseTracker/ExpenseTracker/ExpenseTrackerApp.swift
+++ ExpenseTracker/ExpenseTracker/ExpenseTrackerApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct ExpenseTrackerApp: App {
+    
+    @StateObject var transactionListViewModel = TransactionListViewModel()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(transactionListViewModel)
         }
     }
 }

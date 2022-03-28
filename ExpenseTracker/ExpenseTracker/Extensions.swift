de722feb41563cdad14bb3eb5973ed8c61b5c851
takahiro.tominaga <tt.300607@gmail.com>
2022-03-28 23:27:53 +0900

    created custom colors

---
+++ ExpenseTracker/ExpenseTracker/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by Takahiro Tominaga on 2022/03/28.
+//
+
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}

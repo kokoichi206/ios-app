a7413b2d82c4dde077cd3f7cc3ee5854d8213cb4
takahiro.tominaga <tt.300607@gmail.com>
2022-03-30 22:42:39 +0900

    initial commit

---
+++ CoreDataPrac/CoreDataPrac/CoreDataPracApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataPracApp.swift
+//  CoreDataPrac
+//
+//  Created by Takahiro Tominaga on 2022/03/30.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataPracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

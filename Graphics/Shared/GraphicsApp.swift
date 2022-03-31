72e6fb0a03c0652c010b159bc92e34b4b2997d55
takahiro.tominaga <tt.300607@gmail.com>
2022-03-31 21:43:13 +0900

    initial commit

---
+++ Graphics/Shared/GraphicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphicsApp.swift
+//  Shared
+//
+//  Created by Takahiro Tominaga on 2022/03/31.
+//
+
+import SwiftUI
+
+@main
+struct GraphicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

94c7c8e2f8105fbceebe3e65a8861d0aaafe82c9
takahiro.tominaga <tt.300607@gmail.com>
2022-04-01 03:11:13 +0900

    test

--- Graphics/Shared/ContentView.swift
+++ Graphics/Shared/ContentView.swift
@@ -8,28 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
-
+    
+    enum GameState {
+        case expanding, stopped, reset
+    }
+    
+    enum GameResult {
+        case success, failure
+    }
+    
+    @State var gameState = GameState.reset
+    @State var gameResult = GameResult.success
+    
     @State var originDate = 0.0
+    @State var stoppedDate = 0.0
+    
+    @State var speed = 1.0
+    
+    let targetSpan = 30
 
-    var body: some View {        
+    var body: some View {
         
         TimelineView(.animation) { timeline in
             
             Canvas { context, size in
                 
-                let now = timeline.date.timeIntervalSinceReferenceDate - originDate
+                let now = (gameState == .reset ? 0 : gameState == .stopped ? (stoppedDate - originDate) : timeline.date.timeIntervalSinceReferenceDate - originDate) * speed
                 let angle = Angle(degrees: now * 60)
-                let x = angle.radians
+                let x = min(1.5, angle.radians)
+                
+                let targetRadius = size.width * 0.7
+                
+                context.stroke(Circle().path(in: CGRect(origin: CGPoint(x: 0.5 * (size.width - targetRadius), y: 0.5 * (size.height - targetRadius)), size: CGSize(width: targetRadius, height: targetRadius))), with: .color(.purple), lineWidth: CGFloat(targetSpan))
+                
+                var transparentContext = context
+                transparentContext.opacity = 0.8
                 
                 let radius = size.width * x
                 
-                context.fill(
+                transparentContext.fill(
                     Circle()
                         .path(in: CGRect(origin:
                                             CGPoint(x: 0.5 * (size.width - radius),
                                                     y: 0.5 * (size.height - radius)), size:
                                             CGSize(width: radius, height: radius))), with: .color(.orange)
                 )
+                
+                if x >= 1.5 {
+                    asyncDetached {
+                        // Global actor
+                        stoppedDate = Date.now.timeIntervalSinceReferenceDate
+                        gameState = .stopped
+                    }
+                }
+                
+                if radius > targetRadius + targetSpan || radius < targetRadius - targetSpan {
+                    asyncDetached {
+                        gameResult = .failure
+                    }
+                } else {
+                    asyncDetached {
+                        gameResult = .success
+                    }
+                }
+            }
+        }
+        .onTapGesture {
+            if gameState == .reset {
+                originDate  = Date.now.timeIntervalSinceReferenceDate
+                gameState = .expanding
+            }
+            else if gameState == .expanding {
+                stoppedDate = Date.now.timeIntervalSinceReferenceDate
+                gameState = .stopped
+            }
+            else if gameState == .stopped {
+                originDate  = Date.now.timeIntervalSinceReferenceDate
+                gameState = .expanding
             }
         }
         .task {

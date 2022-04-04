3310194dc9dbd11a75e7ee10651dc4a61cf639de
takahiro.tominaga <tt.300607@gmail.com>
2022-04-05 00:13:48 +0900

    WIP: 動画生成できひん

---
+++ VideoEdit/VideoEdit/MovieCreator.swift
@@ -0,0 +1,180 @@
+//
+//  MovieCreator.swift
+//  VideoEdit
+//
+//  Created by Takahiro Tominaga on 2022/04/02.
+//
+
+import Foundation
+import AVFoundation
+import UIKit
+
+class MovieCreator: NSObject {
+    
+    override init() {
+        
+        super.init()
+    }
+    
+    func create(images:[UIImage],size:CGSize,time:Int) -> URL{
+        
+        //保存先のURL
+        let url = NSURL(fileURLWithPath:NSTemporaryDirectory()).appendingPathComponent("\(NSUUID().uuidString).mp4")
+        // AVAssetWriter
+        guard let videoWriter = try? AVAssetWriter(outputURL: url!, fileType: AVFileType.mov) else {
+            fatalError("AVAssetWriter error")
+        }
+        
+        //画像サイズを変える
+        let width = size.width
+        let height = size.height
+        
+        // AVAssetWriterInput
+        let outputSettings = [
+            AVVideoCodecKey: AVVideoCodecType.h264,
+            AVVideoWidthKey: width,
+            AVVideoHeightKey: height
+            ] as [String : Any]
+        let writerInput = AVAssetWriterInput(mediaType: AVMediaType.video, outputSettings: outputSettings as [String : AnyObject])
+        videoWriter.add(writerInput)
+        
+        // AVAssetWriterInputPixelBufferAdaptor
+        let adaptor = AVAssetWriterInputPixelBufferAdaptor(
+            assetWriterInput: writerInput,
+            sourcePixelBufferAttributes: [
+                kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_32ARGB),
+                kCVPixelBufferWidthKey as String: width,
+                kCVPixelBufferHeightKey as String: height,
+                ]
+        )
+        
+        writerInput.expectsMediaDataInRealTime = true
+        
+        // 動画の生成開始
+        
+        // 生成できるか確認
+        if (!videoWriter.startWriting()) {
+            // error
+            print("error videoWriter startWriting")
+        }
+        
+        // 動画生成開始
+        videoWriter.startSession(atSourceTime: CMTime.zero)
+        
+        // pixel bufferを宣言
+        var buffer: CVPixelBuffer? = nil
+        
+        // 現在のフレームカウント
+        var frameCount = 0
+        
+        // 各画像の表示する時間
+        let durationForEachImage = time
+        
+        // FPS
+        let fps: __int32_t = 60
+        
+        // 全画像をbufferに埋め込む
+        for image in images {
+            print("iyo")
+            
+            if (!adaptor.assetWriterInput.isReadyForMoreMediaData) {
+                break
+            }
+            
+            // 動画の時間を生成(その画像の表示する時間/開始時点と表示時間を渡す)
+            let frameTime: CMTime = CMTimeMake(value: Int64(__int32_t(frameCount) * __int32_t(durationForEachImage)), timescale: fps)
+            //時間経過を確認(確認用)
+            let second = CMTimeGetSeconds(frameTime)
+            print(second)
+            
+            let resize = resizeImage(image: image, contentSize: size)
+            // CGImageからBufferを生成
+            buffer = self.pixelBufferFromCGImage(cgImage: resize.cgImage!)
+            
+            // 生成したBufferを追加
+            if (!adaptor.append(buffer!, withPresentationTime: frameTime)) {
+                // Error!
+                print("adaptError")
+                print(videoWriter.error!)
+            }
+            
+            frameCount += 1
+        }
+        
+        // 動画生成終了
+        writerInput.markAsFinished()
+        videoWriter.endSession(atSourceTime: CMTimeMake(value: Int64((__int32_t(frameCount)) *  __int32_t(durationForEachImage)), timescale: fps))
+        videoWriter.finishWriting(completionHandler: {
+            // Finish!
+            print("movie created.")
+        })
+        
+        return url!
+    }
+    
+    func pixelBufferFromCGImage(cgImage: CGImage) -> CVPixelBuffer {
+        
+        let options = [
+            kCVPixelBufferCGImageCompatibilityKey as String: true,
+            kCVPixelBufferCGBitmapContextCompatibilityKey as String: true
+        ]
+        
+        var pxBuffer: CVPixelBuffer? = nil
+        
+        let width = cgImage.width
+        let height = cgImage.height
+        
+        CVPixelBufferCreate(kCFAllocatorDefault,
+                            width,
+                            height,
+                            kCVPixelFormatType_32ARGB,
+                            options as CFDictionary?,
+                            &pxBuffer)
+        
+        CVPixelBufferLockBaseAddress(pxBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        
+        let pxdata = CVPixelBufferGetBaseAddress(pxBuffer!)
+        
+        let bitsPerComponent: size_t = 8
+        let bytesPerRow: size_t = 4 * width
+        
+        let rgbColorSpace: CGColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pxdata,
+                                width: width,
+                                height: height,
+                                bitsPerComponent: bitsPerComponent,
+                                bytesPerRow: bytesPerRow,
+                                space: rgbColorSpace,
+                                bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+        
+        context?.draw(cgImage, in: CGRect(x:0, y:0, width:CGFloat(width),height:CGFloat(height)))
+        
+        CVPixelBufferUnlockBaseAddress(pxBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return pxBuffer!
+    }
+    
+    private func resizeImage(image:UIImage,contentSize:CGSize) -> UIImage{
+        // リサイズ処理
+        let origWidth  = Int(image.size.width)
+        let origHeight = Int(image.size.height)
+        var resizeWidth:Int = 0, resizeHeight:Int = 0
+        if (origWidth < origHeight) {
+            resizeWidth = Int(contentSize.width)
+            resizeHeight = origHeight * resizeWidth / origWidth
+        } else {
+            resizeHeight = Int(contentSize.height)
+            resizeWidth = origWidth * resizeHeight / origHeight
+        }
+        
+        let resizeSize = CGSize(width:CGFloat(resizeWidth), height:CGFloat(resizeHeight))
+        UIGraphicsBeginImageContext(resizeSize)
+        
+        image.draw(in: CGRect(x:0,y: 0,width: CGFloat(resizeWidth), height:CGFloat(resizeHeight)))
+        
+        let resizeImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    
+        return resizeImage!
+    }
+}
--- VideoEdit/VideoEdit/RectOnVideo.swift
+++ VideoEdit/VideoEdit/RectOnVideo.swift
@@ -30,6 +30,7 @@ class ViewController: UIViewController {
 }
 
 var player = AVPlayer()
+var rectPlayer = AVPlayer()
 
 struct RectOnVideo: View {
     
@@ -198,15 +199,19 @@ class PlayerUIViewPortrait: UIView {
 
 class PlayerUIView: UIView {
     private let playerLayer = AVPlayerLayer()
-    
+    private let rectPlayerLayer = AVPlayerLayer()
+
     init(frame: CGRect, url: URL) {
         super.init(frame: frame)
         
-        player = AVPlayer(url: url)
-        player.play()
-        
-        playerLayer.player = player
-        layer.addSublayer(playerLayer)
+        print("the duration of main movie")
+        print(AVURLAsset(url: url).duration.seconds)
+
+//        player = AVPlayer(url: url)
+//        player.play()
+//
+//        playerLayer.player = player
+//        layer.addSublayer(playerLayer)
         // 完全に中央揃えになってる場合（TODO: 要チェック）
         print("UIScreen ")
         print(UIScreen.main.bounds.width)
@@ -214,8 +219,11 @@ class PlayerUIView: UIView {
         print(playerLayer.videoRect.width)
         print(playerLayer.frame.height)
         
-        var offsetX = (UIScreen.main.bounds.width - playerLayer.frame.width) / 2
-        var offsetY = (UIScreen.main.bounds.height - playerLayer.frame.height) / 2
+//        var offsetX = (UIScreen.main.bounds.width - playerLayer.frame.width) / 2
+//        var offsetY = (UIScreen.main.bounds.height - playerLayer.frame.height) / 2
+//
+        var offsetX = (UIScreen.main.bounds.width) / 2
+        var offsetY = (UIScreen.main.bounds.height) / 2
       
         
         var ratio: CGFloat = 0.0
@@ -246,6 +254,24 @@ class PlayerUIView: UIView {
         print(offsetX)
         print(offsetY)
 
+        let rectUrl = movieTest(offsetX: offsetX, offsetY: offsetY, ratio: ratio)
+        print("rectUrl: \(rectUrl)")
+
+        rectPlayer = AVPlayer(url: rectUrl)
+        print(rectUrl)
+
+        Thread.sleep(forTimeInterval: 1)
+
+        rectPlayer.play()
+        print("size of rectPlayer")
+
+        rectPlayerLayer.player = rectPlayer
+        print(rectPlayer.currentTime())
+        print(rectPlayer.currentItem?.asset.tracks(withMediaType: .video))
+        print(rectPlayerLayer.frame.width)
+
+        layer.addSublayer(rectPlayerLayer)
+
         
 //        // Rectangle
 //        let rectFrame: CGRect = CGRect(x:CGFloat(0), y:CGFloat(0), width:CGFloat(100), height:CGFloat(50))
@@ -257,33 +283,9 @@ class PlayerUIView: UIView {
 //        greenView.backgroundColor = UIColor.green
 //
 //        layer.addSublayer(greenView.layer)
-        let x = 341.0
-        let y = 170.6
-        let rawWidth = 580.0
-        let rawHeight = 341.0
 
-        let startX = offsetX + x * ratio
-        let startY = offsetY + y * ratio
-        let width = rawWidth * ratio
-        let height = rawHeight * ratio
-        let lineWidth = 5.0
-        // 線
-        let line = UIBezierPath()
-        line.move(to: CGPoint(x: startX, y: startY))
-        line.addLine(to:CGPoint(x: startX + width, y: startY))
-        line.addLine(to:CGPoint(x: startX + width, y: startY + height))
-        line.addLine(to:CGPoint(x: startX, y: startY + height))
-        line.addLine(to:CGPoint(x: startX, y: startY))
-        // 終わる
-        line.close()
-
-        let rect = CAShapeLayer()
-        rect.lineWidth = lineWidth
-        rect.fillColor = .none
-        rect.strokeColor = .init(red: 1, green: 0, blue: 0, alpha: 1)
-        rect.path = line.cgPath
         
-        layer.addSublayer(rect)
+//        layer.addSublayer(rect)
         
 //        print(player.currentTime())
     }
@@ -295,6 +297,123 @@ class PlayerUIView: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
         playerLayer.frame = bounds
+    }
+    
+    func movieTest(offsetX: CGFloat, offsetY: CGFloat, ratio: CGFloat) -> URL {
+        let x = 341.0
+        let y = 170.6
+        let rawWidth = 580.0
+        let rawHeight = 341.0
+
+//        let startX = offsetX + x * ratio
+//        let startY = offsetY + y * ratio
+        let startX = x * ratio
+        let startY = y * ratio
+        let width = rawWidth * ratio
+        let height = rawHeight * ratio
+        let lineWidth = 5.0
+        
+        // 線
+        let rect = UIBezierPath()
+        rect.move(to: CGPoint(x: startX, y: startY))
+        rect.addLine(to:CGPoint(x: startX + width, y: startY))
+        rect.addLine(to:CGPoint(x: startX + width, y: startY + height))
+        rect.addLine(to:CGPoint(x: startX, y: startY + height))
+        rect.addLine(to:CGPoint(x: startX, y: startY))
+        // 終わる
+        rect.close()
+
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.lineWidth = lineWidth
+        shapeLayer.fillColor = .none
+        shapeLayer.strokeColor = .init(red: 1, green: 0, blue: 0, alpha: 1)
+        shapeLayer.path = rect.cgPath
+
+//        let diameter: CGFloat = 100
+//        let rect = CGRect(origin: CGPoint.zero,
+//                          size: CGSize(width: diameter, height: diameter))
+//
+//        let shapeLayer = CAShapeLayer()
+//        shapeLayer.fillColor = UIColor.white.cgColor
+//        shapeLayer.lineWidth = 10
+//        shapeLayer.path = CGPath(ellipseIn: rect,
+//                                 transform: nil)
+        
+//        let image = UIImage.imageWithLayer(layer: rect)
+//        let image = UIImage.image(from: rect)
+
+        var renderer = UIGraphicsImageRenderer(size: CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+//        let renderer = UIGraphicsImageRenderer(size: rect.size)
+         renderer = UIGraphicsImageRenderer(size: CGSize(width: 320, height: 320))
+             
+        var image = renderer.image {
+            context in
+            print("renderer.image")
+
+            return shapeLayer.render(in: context.cgContext)
+        }
+        
+        image = UIImage(named: "cat.png")!
+
+        let movie = MovieCreator()
+        //16の倍数
+        let movieSize = CGSize(width:320, height:320)
+        // time / 60 秒表示する
+        let time = 6009
+
+        var imageArray: [UIImage] = [image, image, image, image, image, image, image, image, image]
+//        if image != nil {
+//            imageArray = [image!]
+//            print("imageArray: \(imageArray)")
+//        } else {
+//            print("imageArray: image is nil")
+//        }
+        
+        //動画を生成
+        let movieURL = movie.create(images:imageArray,size:movieSize,time:time)
+        print("movieURL: \(movieURL)")
+        
+//        Thread.sleep(forTimeInterval: 1000)
+        
+        print("end of sleep, WAKE UP!!")
+        
+        let track = AVURLAsset(url: movieURL).tracks(withMediaType: AVMediaType.video).first
+        if track != nil {
+            let size = track?.naturalSize.applying(track!.preferredTransform)
+            print("dims of created movie:")
+            print(size)
+            print("duration of created movie:")
+            print(AVURLAsset(url: movieURL).duration.seconds)
+            print("")
+        }
+        
+        return movieURL
+    }
+}
+
+//extension UIImage {
+//    class func imageWithLayer(layer: CALayer) -> UIImage {
+//        UIGraphicsBeginImageContextWithOptions(layer.bounds.size, layer.isOpaque, 0.0)
+//        layer.render(in: UIGraphicsGetCurrentContext())
+//        let img = UIGraphicsGetImageFromCurrentImageContext() ?? nil
+//        UIGraphicsEndImageContext()
+//        return img!
+//    }
+//}
+extension UIImage {
+    class func image(from layer: CALayer) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(layer.bounds.size,
+        layer.isOpaque, UIScreen.main.scale)
+
+        defer { UIGraphicsEndImageContext() }
+
+        // Don't proceed unless we have context
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return nil
+        }
+
+        layer.render(in: context)
+        return UIGraphicsGetImageFromCurrentImageContext()
     }
 }
 

2efb459f05c93f223f49c51cfead158c9321b51b
takahiro.tominaga <tt.300607@gmail.com>
2022-04-09 18:31:41 +0900

    initial commit

---
+++ SwiftfulMappApp/SwiftfulMappApp/SwiftfulMappAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMappAppApp.swift
+//  SwiftfulMappApp
+//
+//  Created by Takahiro Tominaga on 2022/04/09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMappAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

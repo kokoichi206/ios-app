3650440ed7df6d25c5fcbe0e6376c8b0be168bb0
takahiro.tominaga <tt.300607@gmail.com>
2022-04-10 07:40:30 +0900

    initial commit

---
+++ CustomVideoPlayer/CustomVideoPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomVideoPlayer
+//
+//  Created by Takahiro Tominaga on 2022/04/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomVideoPlayer/CustomVideoPlayer/CustomVideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomVideoPlayerApp.swift
+//  CustomVideoPlayer
+//
+//  Created by Takahiro Tominaga on 2022/04/10.
+//
+
+import SwiftUI
+
+@main
+struct CustomVideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

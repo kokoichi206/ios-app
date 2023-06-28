a14e5e8eeb2b074aecdd71f7677f9f19b867c6b6
takahiro.tominaga <tt.300607@gmail.com>
2023-06-28 23:47:46 +0900

    connected to firebase

--- google signin sample/google signin sample/google_signin_sampleApp.swift
+++ google signin sample/google signin sample/google_signin_sampleApp.swift
@@ -6,12 +6,27 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct google_signin_sampleApp: App {
+
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+      print("Configured Firebase!!!")
+
+    return true
+  }
 }
